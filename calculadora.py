e0f371d3b553ec5e1fb3179dcc23bdb652aafe6d
Elielzin <engsoftware.eliel@gmail.com>
2026-08-20 17:45:55 -0300

    Change

--- calculadora.py
+++ calculadora.py
@@ -1,28 +1,38 @@
-import math
-
 cat1 = input('Digite o valor do primeiro cateto em cm (caso não tenha, digite "X") ')
 cat2 = input('Digite o valor do segundo cateto em cm (caso não tenha, digite "X") ')
 hip = input('Digite o valor da hipotenusa (caso não tenha, digite "X") ')
-try:
+if cat1 and cat2 and hip:
     if cat1 != '' and cat2 != '' and hip != '':
         if cat1.upper() == 'X' and cat2.upper() != 'X' and hip.upper() != 'X':
+            cat2 = cat2.replace(',','.')
+            hip = hip.replace(',', '.')
             cat2 = float(cat2)
             hip = float(hip)
-            cat = math.sqrt((hip ** 2) - (cat2 ** 2))
-            print(f'O valor do cateto é {cat:.2} cm.')
+            cat = ((hip ** 2) - (cat2 ** 2)) ** (1/2)
+            cat = round(cat, 2)
+            cat = str(cat)
+            print(f'O valor do cateto é {cat.replace('.', ',')} cm.')
         elif cat2.upper() == 'X' and cat1.upper() != 'X' and hip.upper() != 'X':
+            cat1 = cat1.replace(',','.')
+            hip = hip.replace(',', '.')
             cat1 = float(cat1)
             hip = float(hip)
-            cat = math.sqrt((hip ** 2) - (cat1 ** 2))
-            print(f'O valor do cateto é {cat:.2} cm.')
+            cat = ((hip ** 2) - (cat1 ** 2)) ** (1/2)
+            cat = round(cat, 2)
+            cat = str(cat)
+            print(f'O valor do cateto é {cat.replace('.', ',')} cm.')
         elif hip.upper() == 'X' and cat2.upper() != 'X' and cat1.upper() != 'X':
+            cat2 = cat2.replace(',','.')
+            cat1 = cat1.replace(',', '.')
             cat1 = float(cat1)
             cat2 = float(cat2) 
-            hipo = math.sqrt((cat1 ** 2) + (cat2 ** 2))
-            print(f'O valor da hipotenusa é {hipo:.2} cm.')
+            hipo = ((cat1 ** 2) + (cat2 ** 2)) ** (1/2)
+            hipo = round(hipo, 2)
+            hipo = str(hipo)
+            print(f'O valor da hipotenusa é {hipo.replace('.', ',')} cm.')
         else:
             print('ERROR: Digite a incógnita corretamente!')
     else:
         print('Digite os  valores corretamente!')
-except ValueError:
+else:
     print('Erro de formatação! Digite os valores corretamente!')
